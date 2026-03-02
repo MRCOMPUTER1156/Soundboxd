af49ed5f461d2c0e413226f73cc213eb0981bf2c
MRCOMPUTER1156 <ciromessi934@gmail.com>
2026-03-02 19:13:29 -0300

    Fix settings ellipsis error

--- musicrate/musicrate/settings.py
+++ musicrate/musicrate/settings.py
@@ -84,8 +84,12 @@ DATABASES = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 
